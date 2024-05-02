9a1c23799e3749fa29c39fb8909df19deceeff84
vaiperlexmar <vaiperlexmar@gmail.com>
2024-05-02 15:48:54 +0300

    style: Make styles for reviews

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -15,14 +15,18 @@ function Bar({ rating }) {
 export default function ReviewCard({ rating, heading, reviewText, account }) {
   return (
     <div className="review-card">
-      <Bar rating={rating} />
+      <div className="review-card__rating-container">
+        <Bar rating={rating} />
+      </div>
       <h3 className="review-card__heading">{heading}</h3>
       <p className="review-card__text">{reviewText}</p>
       <div className="review-card__account">
         <img className="review-card__account-img" src={account.avatar} alt="" />
         <div className="account__textbox">
           <p className="review-card__account-name">{account.name}</p>
-          <p className="review-card__account-nickname">{account.nickname}</p>
+          <a href="#" className="review-card__account-nickname">
+            {account.nickname}
+          </a>
         </div>
       </div>
     </div>
